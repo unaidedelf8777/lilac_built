8cd0a1e09bcdb93c2fedbb34e6bfa82f4e91f9ae
Daniel Smilkov <dsmilkov@gmail.com>
2023-06-01 17:39:37 -0400

    small css fixes in create dataset (#333)

--- web/blueprint/src/routes/datasets/new/+page.svelte
+++ web/blueprint/src/routes/datasets/new/+page.svelte
@@ -56,75 +56,85 @@
   }
 </script>
 
-<div class="mx-auto flex max-w-lg flex-col">
-  <h2>Add dataset</h2>
-  <Form>
-    <FormGroup legendText="Name">
-      <!-- Input field for namespace and name -->
-      <div class="flex flex-row content-start">
-        <TextInput labelText="namespace" bind:value={namespace} invalid={!namespace} />
-        <span class="mx-4 mt-6 text-lg">/</span>
-        <TextInput labelText="name" bind:value={name} invalid={!name} />
-      </div>
-    </FormGroup>
-    <FormGroup legendText="Data Loader">
-      <!-- Radio button for selecting data loader -->
-      {#if $sources.isSuccess}
-        <div>
-          <RadioButtonGroup bind:selected={selectedSource}>
-            {#each $sources.data.sources as source}
-              <RadioButton labelText={source} value={source} />
-            {/each}
-          </RadioButtonGroup>
+<div class="h-full w-full overflow-y-auto py-8">
+  <div class="mx-auto flex max-w-lg flex-col">
+    <h2>Add dataset</h2>
+    <Form class="pt-8">
+      <FormGroup legendText="Name">
+        <!-- Input field for namespace and name -->
+        <div class="flex flex-row content-start">
+          <TextInput labelText="namespace" bind:value={namespace} invalid={!namespace} />
+          <span class="mx-4 mt-6 text-lg">/</span>
+          <TextInput labelText="name" bind:value={name} invalid={!name} />
         </div>
-      {:else if $sources.isError}
-        <InlineNotification
-          kind="error"
-          title="Error"
-          subtitle={$sources.error.message}
-          hideCloseButton
-        />
-      {:else if $sources.isLoading}
-        <RadioButtonSkeleton />
-      {/if}
-
-      {#if $sourcesSchema.isSuccess}
-        {@const schema = $sourcesSchema.data}
-        {#key selectedSource}
-          <!-- Data Loader Fields -->
-          <JsonSchemaForm
-            {schema}
-            hiddenProperties={['/source_name']}
-            bind:value={sourceSchemaValues}
-            bind:validationErrors={errors}
-            customComponents={selectedSource === 'huggingface'
-              ? {
-                  '/dataset_name': DatasetNameInput,
-                  '/split': SplitsInput
-                }
-              : {}}
+      </FormGroup>
+      <FormGroup legendText="Data Loader">
+        <!-- Radio button for selecting data loader -->
+        {#if $sources.isSuccess}
+          <div>
+            <RadioButtonGroup bind:selected={selectedSource}>
+              {#each $sources.data.sources as source}
+                <RadioButton labelText={source} value={source} />
+              {/each}
+            </RadioButtonGroup>
+          </div>
+        {:else if $sources.isError}
+          <InlineNotification
+            kind="error"
+            title="Error"
+            subtitle={$sources.error.message}
+            hideCloseButton
           />
-        {/key}
-      {:else if $sourcesSchema.isError}
-        <InlineNotification
-          kind="error"
-          title="Error"
-          hideCloseButton
-          subtitle={$sourcesSchema.error.message}
-        />
-      {:else if $sourcesSchema.isLoading}
-        <div class="mt-4">
-          <h3 class="text-lg">Schema</h3>
-          <SkeletonText />
-        </div>
-      {/if}
-    </FormGroup>
-    <Button on:click={submit} disabled={errors?.length > 0 || !name || !namespace}>Add</Button>
-  </Form>
+        {:else if $sources.isLoading}
+          <RadioButtonSkeleton />
+        {/if}
+
+        {#if $sourcesSchema.isSuccess}
+          {@const schema = $sourcesSchema.data}
+          {#key selectedSource}
+            <!-- Data Loader Fields -->
+            <JsonSchemaForm
+              {schema}
+              hiddenProperties={['/source_name']}
+              bind:value={sourceSchemaValues}
+              bind:validationErrors={errors}
+              customComponents={selectedSource === 'huggingface'
+                ? {
+                    '/dataset_name': DatasetNameInput,
+                    '/split': SplitsInput
+                  }
+                : {}}
+            />
+          {/key}
+        {:else if $sourcesSchema.isError}
+          <InlineNotification
+            kind="error"
+            title="Error"
+            hideCloseButton
+            subtitle={$sourcesSchema.error.message}
+          />
+        {:else if $sourcesSchema.isLoading}
+          <div class="mt-4">
+            <h3 class="text-lg">Schema</h3>
+            <SkeletonText />
+          </div>
+        {/if}
+      </FormGroup>
+      <Button on:click={submit} disabled={errors?.length > 0 || !name || !namespace}>Add</Button>
+    </Form>
+  </div>
 </div>
 
 <style lang="postcss">
   :global(.bx--form-item) {
     @apply mb-6;
+  }
+
+  :global(.bx--fieldset) {
+    @apply rounded border border-gray-300 p-4;
+  }
+
+  :global(legend.bx--label) {
+    @apply px-2 text-lg;
   }
 </style>
