Add sample weights to the ML model and fix the css overflow problem (#405)

src/concepts/concept.py
@@ -100,7 +100,7 @@ class LogisticEmbeddingModel(BaseModel):
   # The following fields are excluded from JSON serialization, but still pickleable.
   # See `notebooks/Toxicity.ipynb` for an example of training a concept model.
   _model: LogisticRegression = LogisticRegression(
-    class_weight='balanced', C=30, tol=1e-5, warm_start=True, max_iter=1_000, n_jobs=-1)
+    class_weight=None, C=30, tol=1e-5, warm_start=True, max_iter=1_000, n_jobs=-1)
 
   def score_embeddings(self, embeddings: np.ndarray) -> np.ndarray:
     """Get the scores for the provided embeddings."""
@@ -109,11 +109,18 @@ class LogisticEmbeddingModel(BaseModel):
     except NotFittedError:
       return np.random.rand(len(embeddings))
 
-  def fit(self, embeddings: np.ndarray, labels: list[bool]) -> None:
+  def fit(self, embeddings: np.ndarray, labels: list[bool], sample_weights: list[float]) -> None:
     """Fit the model to the provided embeddings and labels."""
     if len(set(labels)) < 2:
       return
-    self._model.fit(embeddings, labels)
+    if len(labels) != len(embeddings):
+      raise ValueError(
+        f'Length of embeddings ({len(embeddings)}) must match length of labels ({len(labels)})')
+    if len(sample_weights) != len(labels):
+      raise ValueError(
+        f'Length of sample_weights ({len(sample_weights)}) must match length of labels '
+        f'({len(labels)})')
+    self._model.fit(embeddings, labels, sample_weights)
 
 
 def draft_examples(concept: Concept, draft: DraftId) -> dict[str, Example]:
@@ -215,14 +222,18 @@ class ConceptModel(BaseModel):
       examples = draft_examples(concept, draft)
       embeddings = np.array([self._embeddings[id] for id in examples.keys()])
       labels = [example.label for example in examples.values()]
-
+      num_pos_labels = len([x for x in labels if x])
+      num_neg_labels = len([x for x in labels if not x])
+      sample_weights = [(1.0 / num_pos_labels if x else 1.0 / num_neg_labels) for x in labels]
       if self._negative_vectors is not None:
+        num_implicit_labels = len(self._negative_vectors)
         embeddings = np.concatenate([self._negative_vectors, embeddings])
-        labels = [False] * len(self._negative_vectors) + labels
+        labels = [False] * num_implicit_labels + labels
+        sample_weights = [1.0 / num_implicit_labels] * num_implicit_labels + sample_weights
 
       model = self._get_logistic_model(draft)
       with DebugTimer(f'Fitting model for "{concept_path}"'):
-        model.fit(embeddings, labels)
+        model.fit(embeddings, labels, sample_weights)
 
       # Synchronize the model version with the concept version.
       model.version = concept.version

src/concepts/db_concept_test.py
@@ -423,7 +423,7 @@ class TestLogisticModel(LogisticEmbeddingModel):
     return np.array([.1])
 
   @override
-  def fit(self, embeddings: np.ndarray, labels: list[bool]) -> None:
+  def fit(self, embeddings: np.ndarray, labels: list[bool], sample_weights: list[float]) -> None:
     pass
 
 
@@ -478,11 +478,13 @@ class ConceptModelDBSuite:
     assert score_embeddings_mock.call_count == 0
     assert fit_mock.call_count == 1
 
-    (called_model, called_embeddings, called_labels) = fit_mock.call_args_list[-1].args
+    (called_model, called_embeddings, called_labels,
+     called_weights) = fit_mock.call_args_list[-1].args
     assert called_model == logistic_model
     np.testing.assert_array_equal(
       called_embeddings, np.array([EMBEDDING_MAP['not in concept'], EMBEDDING_MAP['in concept']]))
     assert called_labels == [False, True]
+    assert called_weights == [1.0, 1.0]
 
     # Edit the concept.
     concept_db.edit('test', 'test_concept',
@@ -498,7 +500,8 @@ class ConceptModelDBSuite:
     assert score_embeddings_mock.call_count == 0
     assert fit_mock.call_count == 2
     # Fit is called again with new points on main only.
-    (called_model, called_embeddings, called_labels) = fit_mock.call_args_list[-1].args
+    (called_model, called_embeddings, called_labels,
+     called_weights) = fit_mock.call_args_list[-1].args
     assert called_model == logistic_model
     np.testing.assert_array_equal(
       called_embeddings,
@@ -507,6 +510,7 @@ class ConceptModelDBSuite:
         EMBEDDING_MAP['a new data point']
       ]))
     assert called_labels == [False, True, False]
+    assert called_weights == pytest.approx([1 / 2, 1.0, 1 / 2])
 
   def test_out_of_sync_draft_model(self, concept_db_cls: Type[ConceptDB],
                                    model_db_cls: Type[ConceptModelDB],
@@ -549,8 +553,8 @@ class ConceptModelDBSuite:
     assert fit_mock.call_count == 3  # Fit is called on both the draft, and main.
 
     # Fit is called again with the same points.
-    ((called_model, called_embeddings, called_labels),
-     (called_draft_model, called_draft_embeddings, called_draft_labels)) = (
+    ((called_model, called_embeddings, called_labels, called_weights),
+     (called_draft_model, called_draft_embeddings, called_draft_labels, called_draft_weights)) = (
        c.args for c in fit_mock.call_args_list[-2:])
 
     # The draft model is called with the data from main, and the data from draft.
@@ -568,12 +572,14 @@ class ConceptModelDBSuite:
       False,
       False
     ]
+    assert called_draft_weights == pytest.approx([1.0, 1 / 3, 1 / 3, 1 / 3])
 
     # The main model was fit without the data from the draft.
     assert called_model == draft_model
     np.testing.assert_array_equal(
       called_embeddings, np.array([EMBEDDING_MAP['not in concept'], EMBEDDING_MAP['in concept']]))
     assert called_labels == [False, True]
+    assert called_weights == pytest.approx([1.0, 1.0])
 
   def test_embedding_not_found_in_map(self, concept_db_cls: Type[ConceptDB],
                                       model_db_cls: Type[ConceptModelDB]) -> None: