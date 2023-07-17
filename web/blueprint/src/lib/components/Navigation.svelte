368478c6da7443c5ab475019c1fe8e3130c64c04
Daniel Smilkov <dsmilkov@gmail.com>
2023-07-17 14:12:23 -0400

    remove tooltips from the navigation buttons (#446)
    https://huggingface.co/spaces/lilacai/daniel_staging

--- web/blueprint/src/lib/components/Navigation.svelte
+++ web/blueprint/src/lib/components/Navigation.svelte
@@ -1,67 +1,11 @@
 <script lang="ts">
-  import {goto} from '$app/navigation';
-  import {Button} from 'carbon-components-svelte';
   import {DataBase, Home, Light, Settings} from 'carbon-icons-svelte';
+  import NavigationButton from './NavigationButton.svelte';
 </script>
 
 <div class="flex w-full flex-col items-center pt-2">
-  <div class="nav-item-container">
-    <Button
-      icon={Home}
-      kind="ghost"
-      expressive
-      iconDescription="Home"
-      tooltipAlignment={'start'}
-      on:click={() => goto('/')}
-    />
-    <div class="nav-icon-text">Home</div>
-  </div>
-  <div class="nav-item-container">
-    <Button
-      icon={Light}
-      kind="ghost"
-      expressive
-      iconDescription="Concepts"
-      tooltipAlignment={'start'}
-      on:click={() => goto('/concepts')}
-    />
-    <div class="nav-icon-text">Concepts</div>
-  </div>
-  <div class="nav-item-container">
-    <Button
-      icon={DataBase}
-      kind="ghost"
-      expressive
-      iconDescription="Datasets"
-      tooltipAlignment={'start'}
-      on:click={() => goto('/datasets')}
-    />
-    <div class="nav-icon-text">Datasets</div>
-  </div>
-  <div class="nav-item-container">
-    <Button
-      icon={Settings}
-      kind="ghost"
-      expressive
-      iconDescription="Datasets"
-      tooltipAlignment={'start'}
-      on:click={() => goto('/settings')}
-    />
-    <div class="nav-icon-text">Settings</div>
-  </div>
+  <NavigationButton href="/" title="Home" icon={Home} />
+  <NavigationButton href="/concepts" title="Concepts" icon={Light} />
+  <NavigationButton href="/datasets" title="Datasets" icon={DataBase} />
+  <NavigationButton href="/settings" title="Settings" icon={Settings} />
 </div>
-
-<style lang="postcss">
-  .nav-item-container {
-    @apply mb-2 flex flex-col items-center;
-  }
-  :global(.nav-item-container .bx--btn) {
-    @apply w-full;
-  }
-  :global(.nav-item-container svg) {
-    margin: auto !important;
-  }
-  .nav-icon-text {
-    @apply text-xs font-light;
-  }
-</style>
---
+++ web/blueprint/src/lib/components/NavigationButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import {Button} from 'carbon-components-svelte';
+  import type {CarbonIcon} from 'carbon-icons-svelte';
+
+  export let href: string;
+  export let title: string;
+  export let icon: typeof CarbonIcon;
+</script>
+
+<div class="nav-item-container">
+  <Button kind="ghost" {href}>
+    <div class="flex w-12 flex-col items-center">
+      <div class="p-2"><svelte:component this={icon} /></div>
+      <div class="nav-icon-text">{title}</div>
+    </div>
+  </Button>
+</div>
+
+<style lang="postcss">
+  .nav-item-container {
+    @apply mb-2 flex flex-col items-center;
+  }
+  :global(.nav-item-container .bx--btn) {
+    @apply w-full;
+  }
+  :global(.nav-item-container svg) {
+    margin: auto !important;
+  }
+  .nav-icon-text {
+    @apply text-xs font-light;
+  }
+</style>
