9bdea3187e5fcff304dbc7b93b8ba9564c2334d3
Nikhil Thorat <nsthorat@gmail.com>
2023-08-29 14:13:32 -0400

    Update publish script to check in pyproject, tag the branch, create a release. (#614)
    NOTE: I haven't tried this yet as it has to run from `main`.

--- scripts/publish_pip.sh
+++ scripts/publish_pip.sh
@@ -10,12 +10,41 @@ if [[ -z "${PYPI_TOKEN}" ]]; then
   exit 1
 fi
 
+CHANGES=`git status --porcelain`
+CUR_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+if [[ "$CUR_BRANCH" != "main" ]]; then
+  echo "Please checkout the main branch before publishing."
+  exit 1
+fi
+
+if [ ! -z "$CHANGES" ];
+then
+  echo "Make sure the main branch has no changes. Found changes:"
+  echo $CHANGES
+  exit 1
+fi
+
 # Build the web server.
+echo "Building webserver..."
 ./scripts/build_server_prod.sh
 
+exit 1
+
+# Upgrade the version in pyproject.
 poetry version patch
+
+NEW_VERSION=`poetry version --short`
+TAG_ID="v$NEW_VERSION"
+
+# PYPI_TOKEN must be set in .env.local.
 poetry config pypi-token.pypi $PYPI_TOKEN
+
+# Build the wheel file.
+echo "Building wheel..."
 poetry build
+
+# Publish to pip.
 
 read -p "Continue (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
@@ -23,4 +52,22 @@ if [ "$CONT" = "y" ]; then
   echo "Published $(poetry version)"
 else
   echo "Did not publish"
+  exit 1
 fi
+
+# Commit directly to main.
+echo "Commiting version in pyproject.toml to main..."
+git commit -a -m "Bump version to $NEW_VERSION."
+git push origin main
+
+# Create the tag and push it.
+echo "Tagging with $TAG_ID..."
+git tag "$TAG_ID" && git push --tags
+
+# Create the release with auto-generated release notes.
+echo "Creating release..."
+gh release create "$TAG_ID" ./dist/*.whl \
+  --generate-notes \
+  --latest \
+  --title "$TAG_ID" \
+  --verify-tag
