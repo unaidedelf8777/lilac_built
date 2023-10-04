6419f23655e4b2379d59cf18bf9544192a13083b
Daniel Smilkov <dsmilkov@gmail.com>
2023-10-04 15:21:38 -0400

    save

--- web/blueprint/src/lib/components/datasetView/FilterPanel.svelte
+++ web/blueprint/src/lib/components/datasetView/FilterPanel.svelte
@@ -3,47 +3,25 @@
   import {getDatasetContext} from '$lib/stores/datasetStore';
   import {getDatasetViewContext} from '$lib/stores/datasetViewStore';
   import {conceptLink} from '$lib/utils';
-  import {displayPath, getSearches, getSort} from '$lib/view_utils';
-  import {
-    deserializePath,
-    formatValue,
-    petals,
-    serializePath,
-    type Path,
-    type Search,
-    type SearchResultInfo,
-    type SearchType,
-    type WebManifest
-  } from '$lilac';
-  import {
-    Button,
-    Modal,
-    Select,
-    SelectItem,
-    SelectItemGroup,
-    SkeletonText
-  } from 'carbon-components-svelte';
-  import {
-    ArrowUpRight,
-    Close,
-    Filter,
-    SortAscending,
-    SortDescending,
-    SortRemove
-  } from 'carbon-icons-svelte';
-  import {hoverTooltip} from '../common/HoverTooltip';
+  import {getSearches} from '$lib/view_utils';
+  import {formatValue, type Search, type SearchType, type WebManifest} from '$lilac';
+  import {Button, Modal, SkeletonText} from 'carbon-components-svelte';
+  import {ArrowUpRight, Filter} from 'carbon-icons-svelte';
   import ConceptView from '../concepts/ConceptView.svelte';
   import AddLabel from './AddLabel.svelte';
   import FilterPill from './FilterPill.svelte';
-  import GroupByDropdown from './GroupByDropdown.svelte';
+  import GroupByDropPill from './GroupByDropPill.svelte';
   import GroupByPanel from './GroupByPanel.svelte';
   import SearchPill from './SearchPill.svelte';
+  import SortDropPill from './SortDropPill.svelte';
 
   export let totalNumRows: number | undefined;
   export let manifest: WebManifest | undefined;
 
   let datasetViewStore = getDatasetViewContext();
   let datasetStore = getDatasetContext();
+
+  $: schema = $datasetStore?.selectRowsSchema?.data?.schema;
 
   let openedConcept: {namespace: string; name: string} | null = null;
   $: concept = openedConcept
@@ -81,64 +59,10 @@
       openedConcept = {namespace: search.concept_namespace, name: search.concept_name};
     }
   }
-
-  // Sorts.
-  $: sort = getSort($datasetStore);
-  let pathToSearchResult: {[path: string]: SearchResultInfo} = {};
-  $: {
-    for (const search of $datasetStore.selectRowsSchema?.data?.search_results || []) {
-      pathToSearchResult[serializePath(search.result_path)] = search;
-    }
-  }
-
-  // Server sort response.
-  $: sortById = sort?.path ? serializePath(sort.path) : '';
-  // Explicit user selection of sort.
-  $: selectedSortBy = $datasetViewStore.query.sort_by;
-
-  $: schema = $datasetStore?.selectRowsSchema?.data?.schema;
-  $: sortItems =
-    schema != null
-      ? [
-          {path: [''], text: 'None', disabled: selectedSortBy == null && sortById !== ''},
-          ...petals(schema)
-            .filter(f => f.dtype != 'embedding' && f.dtype != 'string_span')
-            .map(field => {
-              return {
-                path: field.path,
-                text: displayPath(field.path.slice(1)),
-                disabled: false
-              };
-            })
-        ]
-      : [];
-  $: sortGroups = sortItems.reduce((groups, item) => {
-    const group = item.path[0];
-    (groups[group] = groups[group] || []).push(item);
-    return groups;
-  }, {} as {[key: string]: {path?: Path; text: string; disabled: boolean}[]});
-
-  const selectSort = (ev: CustomEvent<string | number>) => {
-    const selected = ev.detail as string;
-    if (selected === sortById) {
-      return;
-    }
-    datasetViewStore.setSortBy(selected === '' ? null : deserializePath(selected));
-  };
-  const toggleSortOrder = () => {
-    // Set the sort given by the select rows schema explicitly.
-    if (sort != null) {
-      datasetViewStore.setSortBy(sort.path);
-    }
-    datasetViewStore.setSortOrder(sort?.order === 'ASC' ? 'DESC' : 'ASC');
-  };
-  const clearSorts = () => {
-    datasetViewStore.clearSorts();
-  };
 </script>
 
 <div class="mx-5 my-2 flex items-center justify-between">
-  <div class="flex items-center gap-x-8 gap-y-2">
+  <div class="flex items-center gap-x-6 gap-y-2">
     <AddLabel
       addLabelsQuery={{searches, filters}}
       buttonText={'Label all'}
@@ -183,55 +107,8 @@
       {/if}
     </div>
 
-    <GroupByDropdown />
-
-    <!-- Sorting -->
-    <div class="sort-container flex flex-row items-center gap-x-1 md:w-fit">
-      <button
-        use:hoverTooltip={{
-          text:
-            sort?.order === 'ASC'
-              ? 'Sorted ascending. Toggle to switch to descending.'
-              : 'Sorted descending. Toggle to switch to ascending.'
-        }}
-        disabled={sort == null}
-        on:click={toggleSortOrder}
-      >
-        {#if sort?.order == null}
-          <SortRemove />
-        {:else if sort?.order === 'ASC'}
-          <SortAscending />
-        {:else if sort?.order === 'DESC'}
-          <SortDescending />
-        {/if}
-      </button>
-      {#if sortById === ''}
-        Sort by
-      {:else}
-        <Select noLabel size="sm" class="w-60" selected={sortById} on:update={selectSort}>
-          {#each Object.entries(sortGroups) as [groupName, items]}
-            <SelectItemGroup label={groupName}>
-              {#each items as item}
-                <SelectItem
-                  value={item.path != null ? serializePath(item.path) : undefined}
-                  text={item.text}
-                  disabled={item.disabled}
-                />
-              {/each}
-            </SelectItemGroup>
-          {/each}
-        </Select>
-      {/if}
-      {#if selectedSortBy != null}
-        <button
-          use:hoverTooltip={{text: 'Clear sort'}}
-          disabled={sort == null}
-          on:click={clearSorts}
-        >
-          <Close />
-        </button>
-      {/if}
-    </div>
+    <GroupByDropPill />
+    <SortDropPill />
   </div>
   <!-- Number of results. -->
   <div class="flex py-2">
