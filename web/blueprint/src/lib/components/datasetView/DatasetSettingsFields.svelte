869388b0357952763f77d24fd80e554cf1306154
Nikhil Thorat <nikhil@lilacml.com>
2023-11-10 14:10:41 -0800

    Simplify the UI for choosing media fields (#844)
    https://huggingface.co/spaces/lilacai/nikhil_staging
    
    The UI now lets you:
    - Add media fields to a table from a combobox
    - Reorder media fields
    - Add markdown rendering to each media field once it's in the table.
    
    Fixes https://github.com/lilacai/lilac/issues/787

--- web/blueprint/src/lib/components/datasetView/DatasetSettingsFields.svelte
+++ web/blueprint/src/lib/components/datasetView/DatasetSettingsFields.svelte
@@ -2,16 +2,29 @@
   import {queryDatasetSchema, querySettings} from '$lib/queries/datasetQueries';
   import {queryEmbeddings} from '$lib/queries/signalQueries';
   import {getSettingsContext} from '$lib/stores/settingsStore';
+  import {DTYPE_TO_ICON} from '$lib/view_utils';
   import {
+    PATH_WILDCARD,
     ROWID,
+    isLabelField,
     isSignalField,
     pathIsEqual,
     petals,
+    serializePath,
     type DatasetSettings,
     type LilacField
   } from '$lilac';
-  import {Select, SelectItem, SelectSkeleton, SkeletonText} from 'carbon-components-svelte';
-  import FieldList from './FieldList.svelte';
+  import {
+    ComboBox,
+    Select,
+    SelectItem,
+    SelectSkeleton,
+    SkeletonText,
+    Toggle
+  } from 'carbon-components-svelte';
+  import {Add, ArrowDown, ArrowUp, Close} from 'carbon-icons-svelte';
+  import {hoverTooltip} from '../common/HoverTooltip';
+  import {clickOutside} from '../common/clickOutside';
 
   export let namespace: string;
   export let datasetName: string;
@@ -28,12 +41,93 @@
   let markdownMediaFields: LilacField[] | null = null;
   let preferredEmbedding: string | undefined = $appSettings.embedding;
 
-  $: mediaFields =
+  $: mediaFieldOptions =
     $schema.data != null
       ? petals($schema.data).filter(
-          f => f.dtype === 'string' && !pathIsEqual(f.path, [ROWID]) && !isSignalField(f)
+          f =>
+            f.dtype === 'string' &&
+            !pathIsEqual(f.path, [ROWID]) &&
+            !isSignalField(f) &&
+            !isLabelField(f)
         )
       : null;
+  interface MediaFieldComboBoxItem {
+    id: LilacField;
+    text: string;
+  }
+  $: mediaFieldOptionsItems = (mediaFieldOptions || [])
+    .filter(f => !selectedMediaFields?.includes(f))
+    .map(f => {
+      const serializedPath = serializePath(f.path);
+      return {id: f, text: serializedPath};
+    });
+  let mediaFieldsComboBox: ComboBox;
+  function selectMediaField(
+    e: CustomEvent<{
+      selectedItem: MediaFieldComboBoxItem;
+    }>
+  ) {
+    const mediaField = e.detail.selectedItem.id;
+    if (selectedMediaFields == null || selectedMediaFields.includes(mediaField)) return;
+    selectedMediaFields = [...selectedMediaFields, mediaField];
+    mediaFieldsComboBox.clear();
+    addMediaFieldButtonOpen = false;
+  }
+  function removeMediaField(mediaField: LilacField) {
+    if (selectedMediaFields == null) return;
+    selectedMediaFields = selectedMediaFields?.filter(f => f !== mediaField);
+  }
+  function moveMediaFieldUp(mediaField: LilacField) {
+    if (selectedMediaFields == null) return;
+    const index = selectedMediaFields.indexOf(mediaField);
+    if (index <= 0) return;
+    selectedMediaFields = [
+      ...selectedMediaFields.slice(0, index - 1),
+      mediaField,
+      selectedMediaFields[index - 1],
+      ...selectedMediaFields.slice(index + 1)
+    ];
+  }
+  function moveMediaFieldDown(mediaField: LilacField) {
+    if (selectedMediaFields == null) return;
+    const index = selectedMediaFields.indexOf(mediaField);
+    if (index < 0 || index >= selectedMediaFields.length - 1) return;
+    selectedMediaFields = [
+      ...selectedMediaFields.slice(0, index),
+      selectedMediaFields[index + 1],
+      mediaField,
+      ...selectedMediaFields.slice(index + 2)
+    ];
+  }
+  // Markdown.
+  function isMarkdownRendered(field: LilacField) {
+    return markdownMediaFields?.includes(field);
+  }
+  function markdownToggle(e: CustomEvent<{toggled: boolean}>, field: LilacField) {
+    if (e.detail.toggled) {
+      addMarkdownField(field);
+    } else {
+      removeMarkdownField(field);
+    }
+  }
+  function addMarkdownField(field: LilacField) {
+    if (markdownMediaFields == null || markdownMediaFields.includes(field)) return;
+    markdownMediaFields = [...markdownMediaFields, field];
+  }
+  function removeMarkdownField(field: LilacField) {
+    if (markdownMediaFields == null) return;
+    markdownMediaFields = markdownMediaFields.filter(f => f !== field);
+  }
+
+  let addMediaFieldButtonOpen = false;
+  function openMediaFieldComboBox() {
+    addMediaFieldButtonOpen = true;
+    requestAnimationFrame(() => {
+      // comboBox.clear({focus: true}) does not open the combo box automatically, so we
+      // programmatically set it.
+      mediaFieldsComboBox.$set({open: true});
+    });
+  }
 
   $: {
     if ($currentSettings.isFetching) {
@@ -44,14 +138,14 @@
   $: {
     if (
       selectedMediaFields == null &&
-      mediaFields != null &&
+      mediaFieldOptions != null &&
       $currentSettings.data != null &&
       !$currentSettings.isFetching
     ) {
       const mediaPathsFromSettings = ($currentSettings.data.ui?.media_paths || []).map(p =>
         Array.isArray(p) ? p : [p]
       );
-      selectedMediaFields = mediaFields.filter(f =>
+      selectedMediaFields = mediaFieldOptions.filter(f =>
         mediaPathsFromSettings.some(path => pathIsEqual(f.path, path))
       );
     }
@@ -60,14 +154,14 @@
   $: {
     if (
       markdownMediaFields == null &&
-      mediaFields != null &&
+      mediaFieldOptions != null &&
       $currentSettings.data != null &&
       !$currentSettings.isFetching
     ) {
       const mardownPathsFromSettings = ($currentSettings.data?.ui?.markdown_paths || []).map(p =>
         Array.isArray(p) ? p : [p]
       );
-      markdownMediaFields = mediaFields.filter(f =>
+      markdownMediaFields = mediaFieldOptions.filter(f =>
         mardownPathsFromSettings.some(path => pathIsEqual(f.path, path))
       );
     }
@@ -99,14 +193,124 @@
   <SkeletonText />
 {:else}
   <div class="flex flex-col gap-y-6">
-    <section class="flex flex-col gap-y-1">
+    <section class="flex flex-col gap-y-4">
       <div class="text-lg text-gray-700">Media fields</div>
       <div class="text-sm text-gray-500">
         Media fields are text fields that are rendered large in the dataset viewer. They are the
         fields on which you can compute signals, embeddings, search, and label.
       </div>
-      {#if selectedMediaFields != null && mediaFields != null}
-        <FieldList fields={mediaFields} bind:checkedFields={selectedMediaFields} />
+
+      {#if selectedMediaFields != null && mediaFieldOptions != null}
+        <div class="table border-spacing-1 gap-y-2">
+          <div class="table-header-group items-center">
+            <div class="table-row text-xs text-neutral-600">
+              <div class="table-cell" />
+              <div class="table-cell">Field</div>
+              <div
+                use:hoverTooltip={{text: 'Render media field with a markdown renderer.'}}
+                class="table-cell"
+              >
+                Markdown
+              </div>
+              <div class="table-cell" />
+            </div>
+          </div>
+          {#if mediaFieldOptions}
+            <div class="table-row-group">
+              {#each selectedMediaFields as mediaField, i}
+                {@const isSignal = isSignalField(mediaField)}
+                {@const isLabel = isLabelField(mediaField)}
+                {@const moveUpDisabled = i === 0}
+                {@const moveDownDisabled = i === selectedMediaFields.length - 1}
+                <div class="table-row h-10">
+                  <div
+                    class="table-cell w-12 rounded-md p-0.5 align-middle"
+                    class:bg-blue-200={isSignal}
+                    class:bg-teal-100={isLabel}
+                  >
+                    {#if mediaField.dtype}
+                      <svelte:component
+                        this={DTYPE_TO_ICON[mediaField.dtype]}
+                        title={mediaField.dtype}
+                      />
+                    {:else}
+                      <span class="font-mono">{'{}'}</span>
+                    {/if}
+                    {#if mediaField.path.indexOf(PATH_WILDCARD) >= 0}[]{/if}
+                  </div>
+                  <div class="table-cell w-40 align-middle">
+                    {serializePath(mediaField.path)}
+                  </div>
+                  <div class="table-cell w-8 align-middle">
+                    <Toggle
+                      class="mx-auto w-8"
+                      size="sm"
+                      hideLabel={true}
+                      labelA=""
+                      labelB=""
+                      toggled={isMarkdownRendered(mediaField)}
+                      on:toggle={e => markdownToggle(e, mediaField)}
+                    />
+                  </div>
+                  <div class="table-cell">
+                    <div class="flex flex-row gap-x-1">
+                      <div
+                        class="ml-auto align-middle"
+                        use:hoverTooltip={{text: 'Move media field up'}}
+                      >
+                        <button
+                          disabled={moveUpDisabled}
+                          class:opacity-50={moveUpDisabled}
+                          on:click={() => moveMediaFieldUp(mediaField)}
+                        >
+                          <ArrowUp size={16} />
+                        </button>
+                      </div>
+                      <div class="align-middle" use:hoverTooltip={{text: 'Move media field down'}}>
+                        <button
+                          disabled={moveDownDisabled}
+                          class:opacity-50={moveDownDisabled}
+                          on:click={() => moveMediaFieldDown(mediaField)}
+                        >
+                          <ArrowDown size={16} />
+                        </button>
+                      </div>
+                      <div class="align-middle" use:hoverTooltip={{text: 'Remove media field'}}>
+                        <button on:click={() => removeMediaField(mediaField)}>
+                          <Close size={16} />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              {/each}
+            </div>
+          {/if}
+        </div>
+        <div class="h-12">
+          <button
+            on:click={openMediaFieldComboBox}
+            class="flex w-fit flex-row items-center gap-x-2 border border-gray-300"
+            class:hidden={addMediaFieldButtonOpen}
+          >
+            <Add /> Add media field
+          </button>
+          <div
+            class:hidden={!addMediaFieldButtonOpen}
+            use:clickOutside={() => (addMediaFieldButtonOpen = false)}
+          >
+            <ComboBox
+              size="sm"
+              open={false}
+              items={mediaFieldOptionsItems}
+              bind:this={mediaFieldsComboBox}
+              on:select={selectMediaField}
+              shouldFilterItem={(item, value) =>
+                item.text.toLowerCase().includes(value.toLowerCase()) || item.id === 'new-label'}
+              placeholder={'Add a media field'}
+            />
+          </div>
+        </div>
       {:else}
         <SelectSkeleton />
       {/if}
@@ -132,18 +336,6 @@
           </Select>
         {/if}
       </div>
-    </section>
-
-    <section class="flex flex-col gap-y-1">
-      <div class="text-lg text-gray-700">Render media as markdown</div>
-      <div class="text-sm text-gray-500">
-        These media fields will be rendered as markdown in the dataset viewer.
-      </div>
-      {#if selectedMediaFields != null && markdownMediaFields != null}
-        <FieldList fields={selectedMediaFields} bind:checkedFields={markdownMediaFields} />
-      {:else}
-        <SelectSkeleton />
-      {/if}
     </section>
   </div>
 {/if}
