9af076203c4104d37e7f48f61487c9cc59f61062
Nathan Hoos <128712250+unaidedelf8777@users.noreply.github.com>
2023-12-11 13:39:22 -0600

    Update run_server_dev.sh

--- run_server_dev.sh
+++ run_server_dev.sh
@@ -10,7 +10,7 @@ npm run dev --workspace web/blueprint -- --open &
 pid[2]=$!
 
 # Run the FastAPI server.
-export LILAC_PROJECT_DIR='./data'
+export LILAC_PROJECT_DIR='/content/drive/MyDrive/lilac/slimorca_dedup/v1/'
 poetry run uvicorn lilac.server:app --reload --port 5432 --host 0.0.0.0 \
   --reload-dir lilac &
 pid[1]=$!
