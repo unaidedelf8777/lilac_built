unaidedelf8777/lilac_built

commit a71d874c349fdd28fac7c2cb4814c8ee7a8b25a9

Add blog post for diffing and `dataset.map` (#912)
https://lilac-386213--staging-s5a5qv9x.web.app/blog/better-curate.html

Also fix a few bugs along the way:
- add `HNSWLIB_NO_NATIVE` to fix HF deployment on AMD machines
- Fix tiny bug with sort dropdown not reflecting the auto-sort of
semantic and concept search
- Fix a bug where boolean histograms were 1 and 0 instead of false and
true
- Fix a bug where media paths can potentially point to columns we've
manually deleted using `rm -r`
- Fix a bug in single item view when there are 0 results for a filter.
- Fix a bug where the user calls ll.set_project_dir() on directory that
doesn't exist, and/or uses the "~" for homedir.
- Fix a bug with delayed requests with settings modal, when deployed on
HF.
- Fix a bug where we forget the "expanded" state when fetching the next
page in single item view
- Minimize re-rendering of items when scrolling or switching to the next
example

web/blueprint/src/lib/components/datasetView/DatasetLoadingPage.svelte
@@ -128,7 +128,9 @@
                 These settings can be changed from the dataset page at any time.
               </div>
             </div>
-            <DatasetSettingsFields {namespace} {datasetName} bind:settings={newSettings} />
+            {#if newSettings}
+              <DatasetSettingsFields {namespace} {datasetName} bind:newSettings />
+            {/if}
           </section>
         </div>
 

web/blueprint/src/lib/components/datasetView/DatasetSettingsModal.svelte
@@ -46,13 +46,7 @@
   ];
   let labelComboBox: ComboBox;
 
-  $: currentSettings = querySettings(namespace, name);
-  let newTags: Set<string> | null = null;
-  $: {
-    if (newTags == null && $currentSettings.data != null) {
-      newTags = new Set($currentSettings.data.tags);
-    }
-  }
+  $: settingsQuery = querySettings(namespace, name);
 
   const updateSettings = updateDatasetSettingsMutation();
 
@@ -60,8 +54,23 @@
 
   let settingsPage: 'fields' | 'tags' | 'administration' = 'fields';
 
-  let newSettings: DatasetSettings | undefined = undefined;
-  // TODO: bind settings to newSettings
+  function parseSettings(settings: DatasetSettings | undefined): DatasetSettings | null {
+    if (settings == null) return null;
+    const result = JSON.parse(JSON.stringify(settings)) as DatasetSettings;
+    if (result.ui != null) {
+      result.ui.media_paths = result.ui.media_paths?.map(p => (Array.isArray(p) ? p : [p]));
+      result.ui.markdown_paths = result.ui.markdown_paths?.map(p => (Array.isArray(p) ? p : [p]));
+    }
+    return result;
+  }
+  let newSettings: DatasetSettings | null = parseSettings($settingsQuery?.data);
+  $: isFetching = $settingsQuery.isFetching;
+  $: newSettings = isFetching
+    ? null
+    : newSettings == null
+    ? parseSettings($settingsQuery.data)
+    : newSettings;
+
   function submit() {
     if (newSettings == null) return;
     $updateSettings.mutate([namespace, name, newSettings], {
@@ -79,23 +88,23 @@
       selectedItem: TagItem;
     }>
   ) {
-    if (newTags == null) return;
+    if (newSettings == null) return;
+    if (newSettings.tags == null) {
+      newSettings.tags = [];
+    }
     labelComboBox.clear();
-    newTags.add(e.detail.selectedItem.text);
-    newTags = newTags;
-    newSettings = {
-      ...newSettings,
-      tags: Array.from(newTags)
-    };
+    const newTag = e.detail.selectedItem.text;
+    const existingTag = newSettings.tags.find(t => t === newTag);
+    if (existingTag) return;
+    newSettings.tags = [...newSettings.tags, newTag];
   }
+
   function removeTag(tag: string) {
-    if (newTags == null) return;
-    newTags.delete(tag);
-    newTags = newTags;
-    newSettings = {
-      ...newSettings,
-      tags: Array.from(newTags)
-    };
+    if (newSettings == null) return;
+    if (newSettings.tags == null) {
+      newSettings.tags = [];
+    }
+    newSettings.tags = newSettings.tags.filter(t => t !== tag);
   }
 </script>
 
@@ -118,8 +127,8 @@
         />
       </div>
       <div class="flex w-full flex-col gap-y-6 rounded border border-gray-300 bg-white p-4">
-        {#if settingsPage === 'fields'}
-          <DatasetSettingsFields {namespace} datasetName={name} bind:settings={newSettings} />
+        {#if settingsPage === 'fields' && newSettings}
+          <DatasetSettingsFields {namespace} datasetName={name} bind:newSettings />
         {:else if settingsPage === 'tags'}
           <div class="mb-8 flex flex-col gap-y-4">
             <section class="flex flex-col gap-y-1">
@@ -129,9 +138,9 @@
                 panel.
               </div>
             </section>
-            {#if newTags?.size}
+            {#if newSettings?.tags?.length}
               <div class="flex h-8 flex-row">
-                {#each Array.from(newTags || []) as tag}
+                {#each newSettings.tags as tag}
                   <RemovableTag type="purple" on:remove={() => removeTag(tag)}>
                     {tag}
                   </RemovableTag>

Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.11-slim-bullseye
 
 # Allow statements and log messages to immediately appear in the Knative logs
 ENV PYTHONUNBUFFERED True
+# Fixes "invalid instruction" runtime error on AMD machines (specifically HF Upgraded CPU Space).
+ENV HNSWLIB_NO_NATIVE 1
 
 # Adds GCC and other build tools so we can compile hnswlib and other native/C++ deps.
 RUN apt-get update --fix-missing && apt-get install -y --fix-missing build-essential && \

lilac/data/dataset.py
@@ -243,7 +243,7 @@ def column_from_identifier(column: ColumnId) -> Column:
   return Column(path=column)
 
 
-FeatureValue = Union[StrictInt, StrictFloat, StrictBool, StrictStr, StrictBytes, datetime]
+FeatureValue = Union[StrictBool, StrictInt, StrictFloat, StrictStr, StrictBytes, datetime]
 FeatureListValue = list[StrictStr]
 BinaryFilterTuple = tuple[Path, BinaryOp, FeatureValue]
 StringFilterTuple = tuple[Path, StringOp, FeatureValue]

lilac/env.py
@@ -164,6 +164,8 @@ def get_project_dir() -> str:
 
 def set_project_dir(project_dir: Union[str, pathlib.Path]) -> None:
   """Set the project directory."""
+  project_dir = os.path.expanduser(project_dir)
+  os.makedirs(project_dir, exist_ok=True)
   os.environ['LILAC_PROJECT_DIR'] = str(project_dir)
 
 

commit b8265c8191531e10137a8f28929a6167389e8de9

Fix bug with loading dataset and settings. (#957)
https://huggingface.co/spaces/lilacai/nikhil_staging

Broken in https://github.com/lilacai/lilac/pull/912

web/blueprint/src/lib/components/datasetView/DatasetLoadingPage.svelte
@@ -6,6 +6,7 @@
     queryDatasetSchema,
     querySelectRows,
     querySelectRowsSchema,
+    querySettings,
     updateDatasetSettingsMutation
   } from '$lib/queries/datasetQueries';
   import {queryTaskManifest} from '$lib/queries/taskQueries';
@@ -32,6 +33,15 @@
 
   const datasetViewStore = createDatasetViewStore(namespace, datasetName);
   setDatasetViewContext(datasetViewStore);
+
+  $: settingsQuery = loadTaskComplete ? querySettings(namespace, datasetName) : null;
+
+  $: newSettings =
+    $settingsQuery?.isFetching || $settingsQuery?.data == null
+      ? null
+      : newSettings == null
+      ? JSON.parse(JSON.stringify($settingsQuery?.data))
+      : newSettings;
 
   $: schema = loadTaskComplete ? queryDatasetSchema(namespace, datasetName) : null;
   $: selectRowsSchema = loadTaskComplete

web/blueprint/src/lib/components/datasetView/DatasetSettingsModal.svelte
@@ -57,10 +57,6 @@
   function parseSettings(settings: DatasetSettings | undefined): DatasetSettings | null {
     if (settings == null) return null;
     const result = JSON.parse(JSON.stringify(settings)) as DatasetSettings;
-    if (result.ui != null) {
-      result.ui.media_paths = result.ui.media_paths?.map(p => (Array.isArray(p) ? p : [p]));
-      result.ui.markdown_paths = result.ui.markdown_paths?.map(p => (Array.isArray(p) ? p : [p]));
-    }
     return result;
   }
   let newSettings: DatasetSettings | null = parseSettings($settingsQuery?.data);

web/blueprint/src/lib/queries/datasetQueries.ts
@@ -232,7 +232,15 @@ export const infiniteQuerySelectRows = (
   });
 
 export const queryConfig = createApiQuery(DatasetsService.getConfig, DATASETS_CONFIG_TAG);
-export const querySettings = createApiQuery(DatasetsService.getSettings, DATASETS_SETTINGS_TAG);
+export const querySettings = createApiQuery(DatasetsService.getSettings, DATASETS_SETTINGS_TAG, {
+  select: result => {
+    if (result.ui != null) {
+      result.ui.media_paths = result.ui.media_paths?.map(p => (Array.isArray(p) ? p : [p]));
+      result.ui.markdown_paths = result.ui.markdown_paths?.map(p => (Array.isArray(p) ? p : [p]));
+    }
+    return result;
+  }
+});
 export const updateDatasetSettingsMutation = createApiMutation(DatasetsService.updateSettings, {
   onSuccess: () => {
     queryClient.invalidateQueries([DATASETS_SETTINGS_TAG]);