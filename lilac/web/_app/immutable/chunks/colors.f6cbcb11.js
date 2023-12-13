08ef66d3c482ebc0594bba219eb591888b13f335
unaidedelf8777 <thwackyy.y@gmail.com>
2023-12-13 04:07:27 +0000

    init

---
+++ lilac/web/_app/immutable/chunks/colors.f6cbcb11.js
@@ -0,0 +1,1 @@
+const c="#3366cc";function S(O){return`rgba(180,215,255,${O})`}function E(O){let _=0;return O>.5&&(_=.3+(O-.5)/.5*(.8-.3)),S(_)}export{c as L,S as a,E as c};
---
+++ lilac/web/_app/immutable/chunks/navigation.aea35273.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.34e4e2df.js";const e=o("goto");export{e as g};
